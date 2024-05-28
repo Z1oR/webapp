096363d60238eb74254adb82a82329db65dcff72
Z1oR <79359655+Z1oR@users.noreply.github.com>
2024-05-28 22:10:54 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+let tg = window.Telegram.WebApp;
+
+tg.expand();
+
+tg.MainButton.textColor = '#FFFFFF';
+tg.MainButton.color = '#2cab37';
+
+// Telegram.WebApp.onEvent("mainButtonClicked", function(){
+// 	tg.sendData(item);
+// });
+
+
+
+
+const usercard = document.getElementById("usercard")
+usercard.innerHTML = `${tg.initDataUnsafe.user.username}`
+
+
+
+
+
+
